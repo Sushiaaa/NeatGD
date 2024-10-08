72378ee5426a5ac1f5050f664cdd3a732add18f2
Sushiaaa <86648501+Sushiaaa@users.noreply.github.com>
2024-10-08 02:00:52 +0200

    Added generations

--- population.gd
+++ population.gd
@@ -19,27 +19,46 @@ func initiate_population(size:int, input_amount:int, output_amount:int):
 	population[0].connection_innovation = input_amount*output_amount
 
 func speciate(coefficient1:float, coefficient2:float, coefficient3:float):
-	species = [[population[0]]]
+	var first_species = Species.new(population[0]);
+	species.append(first_species)
 	for genome in population.slice(1):
 		var added_to_species:bool = false
 		for specie in species:
-			if specie[0].get_compatibility_distance(genome, coefficient1, coefficient2, coefficient3) <= compatibility_threshold:
-				specie.append(genome)
+			if specie.representative_genome.get_compatibility_distance(genome, coefficient1, coefficient2, coefficient3) <= compatibility_threshold:
+				specie.members.append(genome)
 				added_to_species = true
 				break
 		if !added_to_species:
-			species.append([genome])
+			species.append(Species.new(genome))
 
-func get_shared_fitness(genome:Genome, species:Array) -> float:
-	var sharing_sum:int = 0
-	for i in species:
-		if species.size() == 0:
-			sharing_sum = 1
-		if i != genome:
-			sharing_sum+=sharing(genome, i)
-	return genome.fitness/sharing_sum
+func sort_by_species_fitness(a, b):
+	return a.shared_fitness > b.shared_fitness
 
-func sharing(genome1:Genome, genome2:Genome):
-	if genome1.get_compatibility_distance(genome2, 1.0, 1.0, 1.0) > compatibility_threshold:
-		return 0
-	return 1 
+func new_generation():
+	var species_offsprings:Array
+	
+	var total_shared_fitness:float
+	for specie in range(species.size()):
+		for genome in species[specie].members:
+			var genome_shared_fitness:float
+			genome_shared_fitness = species[specie].get_shared_fitness(genome)
+			genome.explicit_shared_fitness = genome_shared_fitness
+			total_shared_fitness+=genome_shared_fitness
+			species[specie].shared_fitness+=genome_shared_fitness
+
+	species.sort_custom(sort_by_species_fitness)
+	
+	if total_shared_fitness > 0:
+		for i in range(species.size()):
+			var current_total_population:float
+			if i != species.size()-1:
+				species_offsprings.append(round((species[i].shared_fitness/total_shared_fitness)*population.size()))
+				current_total_population+= round((species[i].shared_fitness/total_shared_fitness)*population.size())
+			else:
+				species_offsprings.append(population.size()-current_total_population)
+	var new_population:Array;
+	for i in range(species.size()):
+		print(species[i].members)
+		for j in range(species_offsprings[i]):
+			new_population.append(species[i].members.pick_random().crossover(species[i].members.pick_random()))
+	population = new_population
