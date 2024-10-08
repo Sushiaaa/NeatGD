050a3ce921971b0c5c342181b9d3fe5f173a12f9
Sushiaaa <86648501+Sushiaaa@users.noreply.github.com>
2024-10-08 02:04:31 +0200

    Added function for making layers

--- genome.gd
+++ genome.gd
@@ -11,6 +11,10 @@ var explicit_shared_fitness:float = 0.0;
 #it needs to be shared across all genomes because innovation numbers have to be unique
 static var connection_innovation:int;
 static var node_innovation:int;
+#{input_id:[[output_id, innovation_number], [output_id1, innovation_number1]}
+static var global_connections:Dictionary;
+
+var neural_network:Array = []
 
 func add_connection(in_node:int, out_node:int, weight:float, enabled:bool, innovation_number:int):
 	connection_genes[innovation_number] = ConnectionGene.new(in_node, out_node, weight, enabled, innovation_number)
@@ -26,6 +30,8 @@ func mutate_add_connection():
 	nodes.remove_at(random_in_node_index);
 	
 	var out_node:NodeGene = nodes.pick_random();
+	if creates_cycle(in_node, out_node) == true:
+		return
 	#make sure nodes are not in reverse order
 	#very messy but it works
 	if((in_node.type == Enums.NodeType.HIDDEN and out_node.type == Enums.NodeType.SENSOR) or 
@@ -42,11 +48,24 @@ func mutate_add_connection():
 	
 	var random_weight = randf_range(-1.0,1.0);
 	
+	if global_connections.has(in_node.id):
+		var is_existing = false
+		for i in global_connections[in_node.id]:
+			if i[0] == out_node.id:
+				connection_innovation = i[1]
+				is_existing = true
+		if !is_existing:
+			connection_innovation+=1
+			global_connections[in_node.id].append([out_node.id, connection_innovation])
+	else:
+		connection_innovation+=1
+		global_connections[in_node.id] = [[out_node.id, connection_innovation]]
 	connection_genes[connection_innovation] = (ConnectionGene.new(in_node.id, out_node.id, random_weight, true, connection_innovation))
-	connection_innovation+=1
+	
+
 
 func mutate_add_node():
-	var new_node = NodeGene.new(0, node_innovation);
+	var new_node = NodeGene.new(node_innovation, Enums.NodeType.HIDDEN);
 	node_genes[node_innovation] = new_node;
 	node_innovation+=1
 	
@@ -63,8 +82,9 @@ func mutate_add_node():
 	connection_genes[connection_innovation] = connection_out_new_node
 	connection_innovation+=1
 
-func crossover(other_parent:Genome, is_fitness_equal:bool) -> Genome:
+func crossover(other_parent:Genome) -> Genome:
 	var new_genome:Genome = Genome.new();
+	var is_fitness_equal = true if fitness == other_parent.fitness else false
 	
 	if is_fitness_equal:
 		var other_parent_nodes:Dictionary = other_parent.node_genes;
@@ -159,7 +179,7 @@ func get_compatibility_distance(other_genome:Genome, coefficient1:float, coeffic
 		average_weight_difference = value[2]
 		other_genome_genome_amount+=value[3]
 		this_genome_genome_amount+=value[4]
-
+	
 	if this_genome_genome_amount > other_genome_genome_amount:
 		compatibility_distance = (coefficient1 * excess_genes)/this_genome_genome_amount + (coefficient2 * disjoint_genes)/this_genome_genome_amount + (coefficient3 * average_weight_difference)
 	else:
@@ -211,3 +231,58 @@ func get_excess_and_disjoint_connection_gene_amount_and_average_weight_differenc
 	
 	var average_weight_difference = total_weight_difference/total_shared_weights if total_shared_weights > 0 else 10
 	return[disjoint_amount, excess_amount, average_weight_difference, lower_genome_amount, higher_genome_amount];
+
+func creates_cycle(in_node:NodeGene, out_node:NodeGene):
+	var visited:Dictionary = {out_node: 0};
+	while true:
+		var num_added = 0
+		for connection in connection_genes:
+			if visited.has(connection_genes[connection].in_node) and !visited.has(connection_genes[connection].out_node):
+				if connection_genes[connection].out_node == in_node:
+					return true
+				visited[connection_genes[connection].out_node] = 0
+				num_added+=1
+		if num_added == 0:
+			return false
+
+func make_feed_forward_layers():
+	var layers = []
+	var s = {} 
+
+	for i in node_genes:
+		if node_genes[i].type == Enums.NodeType.SENSOR:
+			s[i] = true
+
+	while true:
+		var candidates = {} 
+
+		for i in connection_genes:
+			var in_node = connection_genes[i].in_node
+			var out_node = connection_genes[i].out_node
+			if s.has(in_node) and not s.has(out_node):
+				candidates[out_node] = true
+
+		var valid_candidates = {}
+
+		for candidate in candidates.keys():
+			var all_inputs_processed = true
+			for j in connection_genes:
+				if connection_genes[j].out_node == candidate:
+					if not s.has(connection_genes[j].in_node):
+						all_inputs_processed = false
+						break
+			if all_inputs_processed:
+				valid_candidates[candidate] = true
+
+		if valid_candidates.size() == 0:
+			break
+
+		var new_layer = []
+		for node in valid_candidates.keys():
+			new_layer.append(node)
+			s[node] = true 
+		layers.append(new_layer)
+	neural_network = layers
+
+func feed_forward():
+	print(neural_network)
